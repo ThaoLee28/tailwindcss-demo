7d2601299a5ae1bf34cc0550e1a7f5b315fb4a5c
Thao Le <thuthao.le2809@gmail.com>
2018-10-04 16:54:21 +0700

    include jquery

---
+++ js/main.js
@@ -0,0 +1,20 @@
+jQuery(function($) {
+    "use strict";
+
+    var SLZ = window.SLZ || {};
+
+    /*=======================================
+    =             MAIN FUNCTION             =
+    =======================================*/
+
+    SLZ.mainFunction = function() {
+        /*=======================================
+        =    SHOW/HIDE SUB MENU MOBILE         =
+        =======================================*/
+        
+    };
+    $(document).ready(function() {
+        SLZ.mainFunction();
+    });
+    
+});
