7f3a96dfe71f057776b6d42a50947ec89df98f76
Thao Le <thuthao.le2809@gmail.com>
2018-10-04 17:30:45 +0700

    editcl click menu-moblie

--- js/main.js
+++ js/main.js
@@ -11,7 +11,23 @@ jQuery(function($) {
         /*=======================================
         =    SHOW/HIDE SUB MENU MOBILE         =
         =======================================*/
-        
+        var menumobile = function () {
+            $('.btn-menu ').on('click', function () {
+                if ($('.menu-mobile').hasClass('hidden')) {
+                    $('.menu-mobile').removeClass('hidden');
+
+                } else {
+                    $('.menu-mobile').addClass('hidden');
+                }
+                return false;
+            });
+        };
+        menumobile();
+        $(document).click(function (e) {
+            if ($('.menu-mobile').has(e.target).length === 0 && !$('.menu-mobile').is(e.target) && !$(e.target).is('.menu-mobile')) {
+                $('.menu-mobile').addClass('hidden');
+            }
+        });
     };
     $(document).ready(function() {
         SLZ.mainFunction();
